574ada7a198870aadf1402bf2754f8ef5857e4f8
bahri444 <bahrysaipul266@gmail.com>
2022-03-02 00:32:35 -0800

    Portofolio

---
+++ assets/js/apps.js
@@ -0,0 +1,21 @@
+/*menu show y*/ 
+/*menu show*/ 
+/*validate if content exsist*/ 
+/*menu Hide*/ 
+/*validate if content exsist*/
+/*remove menu mobile*/ 
+/*accrodian skills*/
+/*qualification skills*/ 
+/*qualification tab*/ 
+/**/ 
+/**/ 
+/**/ 
+/**/ 
+/**/ 
+/**/ 
+/**/ 
+/**/ 
+/**/ 
+/**/ 
+/**/ 
+/**/ 
